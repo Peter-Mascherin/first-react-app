fed62f743bc17ba97e4308eee116aeed954de159
= <=>
2021-08-21 19:19:17 -0400

    first react commit , added h3 tag

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3>Hello Buddy this works</h3>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,11 @@ function App() {
           Learn React
         </a>
       </header>
+
+      
     </div>
+
+    
   );
 }
 
